0dd70c5a24c985c5ac69b33171489a42c810873d
Simon <simon@jampot.design>
2016-08-18 20:50:01 +0000

    streams update

--- alphabet.js
+++ alphabet.js
@@ -19,18 +19,4 @@ Alphabet.prototype._read = function() {
     }
 };
 
-var Alphabet = require('./alphabet');
-var Cache = require('./cache');
-var alpha = new Alphabet();
-var cache = new Cache('alpha1');
-
-alpha.pipe(cache);
-
-cache.on('finish', function() {
-    console.log('Cache store:');
-    for (var key in Cache.store) {
-        console.log(key, ':', Cache.store[key]);
-    }
-});
-
 module.exports = Alphabet;
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var Alphabet = require('./alphabet');
+var Cache = require('./cache');
 var alpha = new Alphabet();
-alpha.on('data', function(chunk) {
-    console.log(chunk.toString());
+var cache = new Cache('alpha1');
+
+alpha.pipe(cache);
+
+cache.on('finish', function() {
+    console.log('Cache store:');
+    for (var key in Cache.store) {
+        console.log(key, ':', Cache.store[key]);
+    }
 });
